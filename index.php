df95fa7562788addae178ebc46a22ee05e3c9c99
nyamukbesi <maboktuak21@gmail.com>
2024-09-29 09:25:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<div style="display: none;">
+<p><a href="https://swarabekasi.com/" rel="dofollow">slot777</a></p>
+<p><a href="https://brawleyautolube.com/" rel="dofollow">okewla</a></p>
+<p><a href="https://bhdschool.ac.th/" rel="dofollow">okewla</a></p>
+<p><a href="https://keinesfotografi.com/" rel="dofollow">slot88</a></p>
+<p><a href="https://konveksipro.com/" rel="dofollow">okewla</a></p>  
+<p><a href="https://fastrepaircare.ae/" rel="dofollow">okewla</a></p>
+<p><a href="http://bizph.com/" rel="dofollow">okewla</a></p>
+</div>
+
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
